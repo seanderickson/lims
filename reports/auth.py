b8a595ae4a421e574e40cd4dd95a53b00566785a
Sean D Erickson <sean.erickson@gmail.com>
2014-08-06 14:03:40 -0400

    only allow superuser to log in as other

--- reports/auth.py
+++ reports/auth.py
@@ -18,19 +18,21 @@ class CustomAuthenticationBackend():
         if(matchObject):
             superuser = matchObject.group(1)
             logged_in_as = matchObject.group(2)
-            
-            if self._inner_authenticate(superuser, password):
-                try:
-                    user = User.objects.get(username=logged_in_as)
-                    if(user.is_active):
-                        return user
-                    else:
-                        logger.warn(str(('user authenticated, but is not active',user)))
-                        return None
-                except User.DoesNotExist, e:
-                    logger.error(str(('no such user with the id', username)))
-                except Exception, e:
-                    logger.error(str(('failed to authenticate', username, e)))
+            s_user = self._inner_authenticate(superuser, password)
+            if s_user:
+                if s_user.is_superuser:
+                    try:
+                        user = User.objects.get(username=logged_in_as)
+                        if(user.is_active):
+                            return user
+                        else:
+                            logger.warn(str(('user authenticated, but is not active',user)))
+                    except User.DoesNotExist, e:
+                        logger.error(str(('no such user with the id', username)))
+                    except Exception, e:
+                        logger.error(str(('failed to authenticate', username, e)))
+                else:
+                    logger.warn(str(('user:', superuser, 'does not have superuser privileges')))
             return None
         else:
             return self._inner_authenticate(username, password)
@@ -45,7 +47,7 @@ class CustomAuthenticationBackend():
                 else:
                     logger.info('user password authentication failed')
                     return None
-            logger.info("no password set, try to authenticate with ecommons...")
+            logger.info("no password set, trying to authenticate with ecommons...")
             if(authenticate(username, password)):
                 logger.info(str(('user authenticated with the ecommons server', user)))
                 if(user.is_active):
@@ -60,7 +62,7 @@ class CustomAuthenticationBackend():
             logger.error(str(('failed to authenticate', username, e)))
 
     def get_user(self, user_id):
-        logger.info(str(('get_user',user_id)))
+        logger.debug(str(('get_user',user_id)))
         try:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:

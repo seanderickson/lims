0a631d7e4b83082f3c95e3089bcd1527154cf2a2
Sean Erickson <sean.erickson@gmail.com>
2015-11-01 20:33:39 -0500

    remove migration 0018

--- migration.sh
+++ migration.sh
@@ -299,13 +299,7 @@ function migratedb {
       $DJANGO_CMD migrate db $migration >>"$LOGFILE" 2>&1 || error "db $migration failed: $?"
       echo "migration $migration complete: $(ts)" >> "$LOGFILE"
     fi
-    migration='0018'
-    if [[ ! $completed_migrations =~ $migration ]]; then
-      echo "migration $migration: $(ts) ..." >> "$LOGFILE"
-      $DJANGO_CMD migrate db $migration >>"$LOGFILE" 2>&1 || error "db $migration failed: $?"
-      echo "migration $migration complete: $(ts)" >> "$LOGFILE"
-    fi
-    
+
     # substance ID generation left until last
     migration='0099' 
     if [[ ! $completed_migrations =~ $migration ]]; then

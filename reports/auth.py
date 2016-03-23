536138c13c65a4b29dde7d0053c2c7bf0badb961
Sean Erickson <sean.erickson@gmail.com>
2016-03-22 21:28:15 -0400

    allow proxy login for members of "usersAdmin"

--- reports/auth.py
+++ reports/auth.py
@@ -7,6 +7,9 @@ from django.core.exceptions import ValidationError
 
 logger = logging.getLogger(__name__)
 
+USER_PROXY_LOGIN_PATTERN = re.compile(r'(\w+)\:(\w+)')
+USER_PROXY_ADMIN_GROUP = 'usersAdmin'
+
 class CustomAuthenticationBackend():
     """
     Authenticate users - if auth_user has a valid password, use that, otherwise,
@@ -16,28 +19,39 @@ class CustomAuthenticationBackend():
     def authenticate(self, username=None, password=None):
         logger.info('find and authenticate the user: %s', username)
 
-        # Test for superuser logging in as another user
-        matchObject = re.match(r'(\w+)\:(\w+)', username)
+        # Proxy Login 
+        matchObject = USER_PROXY_LOGIN_PATTERN.match(username)
         if(matchObject):
             superuser = matchObject.group(1)
             logged_in_as = matchObject.group(2)
             s_user = self._inner_authenticate(superuser, password)
             if s_user:
-                if s_user.is_superuser:
+                is_allowed = s_user.is_superuser
+                if not is_allowed:
+                    try:
+                        is_allowed = (
+                            s_user.userprofile.usergroup_set
+                                .filter(name=USER_PROXY_ADMIN_GROUP).exists())
+                        if is_allowed:
+                            logger.info('user: %r is in the group: %r',
+                                superuser,USER_PROXY_ADMIN_GROUP)
+                    except Exception:
+                        logger.info('user %r is not in the %r group',
+                            superuser,USER_PROXY_ADMIN_GROUP)
+                if is_allowed:
                     try:
                         user = User.objects.get(username=logged_in_as)
-                        if(user.is_active):
-                            return user
-                        else:
-                            msg = 'user authenticated, but is not active: %r' % user
-                            logger.warn(msg)
-                            raise ValidationError(msg)
+                        logger.info('logged in super user %r as %r',
+                            superuser, logged_in_as)
+                        return user
                     except User.DoesNotExist, e:
                         msg = 'no such user with the id: %r' % username
                         logger.warn(msg)
                         raise ValidationError(msg)
                 else:
-                    msg = 'user: %r does not have superuser privileges' % superuser
+                    msg = (
+                        'logging in as another user requires superuser privileges'
+                        ', user: %r') % superuser
                     logger.warn(msg)
                     raise ValidationError(msg)
             return None
@@ -50,12 +64,12 @@ class CustomAuthenticationBackend():
             user = User.objects.get(username=username)
             if user.has_usable_password():
                 if(user.check_password(password)):
+                    logger.info('logged in user %r using password',username)
                     return user
                 else:
                     msg = 'user password authentication failed: %r' % username
                     logger.info(msg)
                     raise ValidationError(msg)
-            logger.info("no password set, trying to authenticate with ecommons...")
             if(authenticate(username, password)):
                 logger.info('user %r authenticated with the ecommons server', user)
                 if(user.is_active):

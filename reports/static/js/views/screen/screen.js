4efe12d951bb370f42d0f9d89462ba9413a29f23
Sean Erickson <sean.erickson@gmail.com>
2016-06-01 00:51:10 -0400

    add screen result excluded column selector options

--- reports/static/js/views/screen/screen.js
+++ reports/static/js/views/screen/screen.js
@@ -1240,17 +1240,19 @@ define([
       var createResults = function(schemaResult){
         
           
-        var options = [];
-        _.each(schemaResult['fields'],function(field){
-          var dc_col_key = 'dc_' + self.model.key + '_';
-          console.log('consider: ', field);
-          if (field['key'].indexOf(dc_col_key)>-1){
-            var option_name = field['key'].substring(dc_col_key.length);
-            options.unshift([option_name, option_name]);
-          }
-        });
-        schemaResult['fields']['excluded']['vocabulary'] = options;
-        console.log('custom exclude options', options);
+        if (_.has(schemaResult['fields'], 'excluded')){
+          var options = [];
+          _.each(schemaResult['fields'],function(field){
+            var dc_col_key = 'dc_' + self.model.key + '_';
+            console.log('consider: ', field);
+            if (field['key'].indexOf(dc_col_key)>-1){
+              var option_name = field['key'].substring(dc_col_key.length);
+              options.unshift([option_name, option_name]);
+            }
+          });
+          schemaResult['fields']['excluded']['vocabulary'] = options;
+          console.log('custom exclude options', options);
+        }
         
         var initialSearchHash;
         view = new ListView({ options: {

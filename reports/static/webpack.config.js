a0e719f4b8c2f5fb4425c7701f4d055b3fa9b21f
Sean Erickson <sean.erickson@gmail.com>
2016-06-29 19:55:58 -0400

    replace image-webpack-loader with url-loader: pngquant: /lib/libc.so.6: version GLIBC_2.14 not found

--- reports/static/webpack.config.js
+++ reports/static/webpack.config.js
@@ -34,13 +34,14 @@ module.exports = {
       { test: require.resolve('jquery'), loader: 'expose?jQuery!expose?$' },
       { test: /\.(html|json)$/, loaders: ['raw'], exclude: /node_modules/ },
       { test: /\.css$/, loaders: ['style', 'css'] },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-          'file?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-        ]
-      },
+//      {
+//        test: /\.(jpe?g|png|gif|svg)$/i,
+//        loaders: [
+//          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+//          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+//        ]
+//      },
+      { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'url-loader?limit=8192' }, // inline base64 URLs for <=8k images, direct URLs for the rest
       {
         test: /\.(eot|svg|ttf|woff|woff2)$/,
         loader: 'file?name=css/fonts/[name].[ext]'

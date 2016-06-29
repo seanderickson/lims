5a3d51b63732239b7e3c63cd7b02a6c7f11fc6f0
Sean Erickson <sean.erickson@gmail.com>
2016-06-29 12:31:19 -0400

    change default STATIC_URL to match production

--- lims/base_settings.py
+++ lims/base_settings.py
@@ -70,7 +70,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = '/_static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (

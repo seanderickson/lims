e7c1ed1c8a1910c0719b02f20d32d2a5fe3726f5
Sean Erickson <sean.erickson@gmail.com>
2017-05-02 15:03:26 -0400

    updates to use custom bootstrap build with webpack

--- reports/static/js/main.js
+++ reports/static/js/main.js
@@ -5,8 +5,9 @@
 // workaround for libraries that need jquery $ in webpack
 //window.$ = window.jQuery = require('jquery');  
 
+require('css/external/bootstrap_custom_build/css/bootstrap.css');
 //require('bootstrap/dist/css/bootstrap.css');
-require('css/external/bootstrap.css');
+
 require('backgrid/lib/backgrid.css');
 require('backgrid-paginator/backgrid-paginator.css');
 require('backgrid-filter/backgrid-filter.css');

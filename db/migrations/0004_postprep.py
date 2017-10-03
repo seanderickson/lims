168ecfeea120cc995e8aa0512f2c9194c526604e
Sean Erickson <sean.erickson@gmail.com>
2017-10-02 23:35:26 -0400

    fix missing new migration

---
+++ db/migrations/0004_postprep.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import csv
+from datetime import timedelta
+import datetime
+import json
+import logging
+import os
+
+from django.contrib.auth.models import User, UserManager
+from django.db import migrations, models
+import pytz
+
+from db.support.data_converter import default_converter
+from lims.base_settings import PROJECT_ROOT
+
+
+logger = logging.getLogger(__name__)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('db', '0003_db_migration_prep'),
+        ('auth', '0001_initial')
+    ]
+
+    operations = [
+        # NOTE: placing these schema migrations here for convenience (so that
+        # they do not conflict with data migrations in 0003, 0004, 0007)
+        migrations.AddField(
+            model_name='screensaveruser',
+            name='lab_head',
+            field=models.ForeignKey(
+                related_name='lab_members', to='db.ScreensaverUser', null=True),
+        ),
+        migrations.AddField(
+            model_name='screensaveruser',
+            name='lab_affiliation',
+            field=models.ForeignKey(related_name='lab_heads', 
+                to='db.LabAffiliation', null=True),
+        ),
+        migrations.AddField(
+            model_name='screensaveruser',
+            name='rnai_data_sharing_level',
+            field=models.IntegerField(null=True),
+        ),
+        migrations.AddField(
+            model_name='screensaveruser',
+            name='sm_data_sharing_level',
+            field=models.IntegerField(null=True),
+        ),
+        
+    ]

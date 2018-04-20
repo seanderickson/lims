301a7e510320e8653bdf2c27a22824e8eca0ca10
Sean D Erickson <sean.erickson@gmail.com>
2018-04-19 23:42:16 -0400

    updates for o2

--- utils.sh
+++ utils.sh
@@ -9,6 +9,19 @@ function error () {
   exit 1
 }
 
+function read_auth_file() {
+  auth_file=$1
+  if [[ ! -e $auth_file ]]; then
+    error "apache auth file not found at: \"$auth_file\""
+  fi
+
+#set -x
+
+  for x in `sed  's/SetEnv\s\+\(\S\+\)\s\+\(.*\)$/\1=\2/;tx;d;:x' $auth_file `; do
+    export $x  
+  done
+}
+
 function invenv {
   local pyex="$VENV/bin/python"
   if [[ -z $VENV || ! -x $pyex ]]; then
@@ -24,12 +37,39 @@ function maybe_activate_virtualenv {
     error 'assertion error'
   fi
 
-  if [[ ! -e $VENV ]]; then
-    virtualenv --no-site-packages --distribute $VENV
-  fi
+#  if [[ ! -e $VENV ]]; then
+#    virtualenv --no-site-packages --distribute $VENV
+#  fi
   VENV="$($REALPATH $VENV)"
 
   if invenv; then return; fi
+
+  module load gcc/6.2.0
+  module load python/2.7.12
+  module load perl
+
+  source "$VENV/bin/activate" || error "failed to activate virtualenv: $?"
+
+  if invenv; then return; fi
+
+  error 'failed to activate virtualenv (reason unknown)'
+}
+
+function maybe_activate_virtualenv_orchestra {
+  if [[ -z $VENV ]]; then
+    error 'assertion error'
+  fi
+
+#  if [[ ! -e $VENV ]]; then
+#    virtualenv --no-site-packages --distribute $VENV
+#  fi
+  VENV="$($REALPATH $VENV)"
+
+  if invenv; then return; fi
+
+#  module load gcc/6.2.0
+#  module load python/2.7.12
+#  module load perl
 
   source "$VENV/bin/activate" || error "failed to activate virtualenv: $?"
 

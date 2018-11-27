3808be4a9d1d98872fa9ea07482337209febf2c2
Sean D. Erickson <sean.erickson@gmail.com>
2018-11-27 02:15:20 -0500

    fix visibility of show_archived_control

--- reports/static/js/views/library/libraryListView.js
+++ reports/static/js/views/library/libraryListView.js
@@ -18,41 +18,40 @@ function($, _, Backbone, layoutmanager, Iccbl, appModel, ListView) {
       var resource = args.resource;
       console.log('LibraryListView', args);
 
+      var show_archived_control = $([
+        '<label class="checkbox-inline pull-left" ',
+        '   title="Show archived libraries" >',
+        '  <input type="checkbox">Show archived libraries</input>&nbsp;',
+        '</label>'
+        ].join(''));
+      show_archived_control.css('margin-left','10px');
+      
       if (appModel.hasPermission(resource.key, 'read')){
         var extraControls = args.extraControls || [];
         args.extraControls = extraControls;
       
-        var show_archived_control = $([
-          '<label class="checkbox-inline pull-left" ',
-          '   title="Show archived libraries" >',
-          '  <input type="checkbox">Show archived libraries</input>&nbsp;',
-          '</label>'
-          ].join(''));
-        show_archived_control.css('margin-left','10px');
-
         extraControls.push(show_archived_control);
       }      
       LibraryListView.__super__.initialize.apply(this, arguments);      
     
-      var initialSearchHash = self.listModel.get(appModel.URI_PATH_SEARCH);
-      var initial_show_archived = 
-        _.result(initialSearchHash, appModel.API_PARAM_SHOW_ARCHIVED, false);
-      if (initial_show_archived && initial_show_archived.toLowerCase()=='true'){
-        show_archived_control.find('input[type="checkbox"]').prop('checked',true);
-      }
-      show_archived_control.find('input[type="checkbox"]').change(function(e) {
-        var searchHash = _.clone(self.listModel.get(appModel.URI_PATH_SEARCH));
-        if (e.target.checked) {
-          searchHash[appModel.API_PARAM_SHOW_ARCHIVED] = 'true';
-        } else {
-          searchHash[appModel.API_PARAM_SHOW_ARCHIVED] = 'false';
+      if (appModel.hasPermission(resource.key, 'read')){
+        var initialSearchHash = self.listModel.get(appModel.URI_PATH_SEARCH);
+        var initial_show_archived = 
+          _.result(initialSearchHash, appModel.API_PARAM_SHOW_ARCHIVED, false);
+        if (initial_show_archived && initial_show_archived.toLowerCase()=='true'){
+          show_archived_control.find('input[type="checkbox"]').prop('checked',true);
         }
-        self.listModel.set(appModel.URI_PATH_SEARCH,searchHash);
-      });
+        show_archived_control.find('input[type="checkbox"]').change(function(e) {
+          var searchHash = _.clone(self.listModel.get(appModel.URI_PATH_SEARCH));
+          if (e.target.checked) {
+            searchHash[appModel.API_PARAM_SHOW_ARCHIVED] = 'true';
+          } else {
+            searchHash[appModel.API_PARAM_SHOW_ARCHIVED] = 'false';
+          }
+          self.listModel.set(appModel.URI_PATH_SEARCH,searchHash);
+        });
         
-    
-    
-    
+      }    
     },
     
     afterRender: function(){
